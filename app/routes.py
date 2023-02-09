b6db1f63c48c97d32bd1a8641f23929c0d6a8aa9
toshiroc <toshirocotta@gmail.com>
2023-02-09 23:57:03 +0000

    Changes to files

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,18 @@
 from app import app
 from flask import render_template, request
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/results', methods=['GET', 'POST'])
+@app.route("/results", methods=["POST"])
 def results():
-    dish = request.form.get('dish')
-    health_score = calculate_health_score(dish)
-    return render_template('results.html', dish=dish, health_score=health_score)
+    dish = request.form["dish"]
+    # Scrape the internet for nutritional information of the dish
+    # Calculate the health score of the dish
+    # Replace this line with your own code
+    health_score = "Your health score for the dish '{}' will go here".format(dish)
+    return render_template("results.html", dish=dish, health_score=health_score)
 
-def calculate_health_score(dish):
-    # Your code to calculate health score goes here
-    return health_score
+if __name__ == "__main__":
+    app.run(debug=True)
